ae1081b37d98d2ef0fd64be47dbfbf172921e457
zillakot <markus.korpinen@gmail.com>
2015-08-31 23:35:45 +0300

    input helper added

---
+++ Assets/Scripts/InputHelper.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UniRx;
+
+public static class InputHelper {
+	public static IObservable<Vector3> MouseClickStream(){
+		return Observable.EveryUpdate()
+   	 		.Where(_ => Input.GetMouseButtonDown(0))
+			.Select(_ => Input.mousePosition);
+	}
+	
+	public static IObservable<Vector3> MouseUpStream(){
+		return Observable.EveryUpdate()
+   	 		.Where(_ => Input.GetMouseButtonUp(0))
+			.Select(_ => Input.mousePosition);
+	}
+	
+	public static IObservable<Vector3> MouseMoveStream(){
+		return Observable.EveryUpdate().Select(_ => Input.mousePosition).DistinctUntilChanged();
+	}
+	
+	public static IObservable<IList<Vector3>> MouseDoubleClickStream(){
+		return MouseClickStream()
+			.Buffer(MouseClickStream()
+				.Throttle(TimeSpan
+					.FromMilliseconds(250)))
+    		.Where(xs => xs.Count >= 2);
+	}
+	
+	public static IObservable<Vector3> MouseDragStream(){
+		return MouseClickStream().SelectMany(_ => {
+    		return MouseMoveStream()
+        		.TakeUntil(MouseUpStream());
+		});
+	}
+}
